079a64fe584a470c71f0e406d64a157dd2442a30
aadigurjarr700 <aditya.gurjar_cs22@gla.ac.in>
2026-01-09 15:08:34 +0530

    [aditya] solved.fitness tracker app

---
+++ oops-csharp-practice/scenario-based/FitnessTrackerApp/CardioWorkout.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BridgeLabzTraining.oops_csharp_practice.scenario_based.fitness_tracker
+{
+    public class CardioWorkout : Workout
+    {
+        public override void TrackWorkout()
+        {
+            Console.WriteLine("Cardio workout selected.");
+        }
+
+        public override int CalculateCalories()
+        {
+            return durationMinutes * 8; // calories per minute
+        }
+    }
+}
---
+++ oops-csharp-practice/scenario-based/FitnessTrackerApp/FitTrack.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BridgeLabzTraining.oops_csharp_practice.scenario_based.fitness_tracker
+{
+   
+
+    class FitTrack
+    {
+        static void Main()
+        {
+            Console.Write("Enter User Name: ");
+            string name = Console.ReadLine();
+
+            UserProfile user = new UserProfile(name);
+
+            int choice;
+            do
+            {
+                Console.WriteLine("\n---- FitTrack Menu ----");
+                Console.WriteLine("1. Cardio Workout");
+                Console.WriteLine("2. Strength Workout");
+                Console.WriteLine("3. Exit");
+                Console.Write("Enter choice: ");
+                choice = int.Parse(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1:
+                        CardioWorkout cardio = new CardioWorkout();
+                        Console.Write("Enter duration in minutes: ");
+                        cardio.DurationMinutes = int.Parse(Console.ReadLine());
+                        user.PerformWorkout(cardio);
+                        break;
+
+                    case 2:
+                        StrengthWorkout strength = new StrengthWorkout();
+                        Console.Write("Enter duration in minutes: ");
+                        strength.DurationMinutes = int.Parse(Console.ReadLine());
+                        user.PerformWorkout(strength);
+                        break;
+
+                    case 3:
+                        Console.WriteLine("Exiting FitTrack...");
+                        break;
+
+                    default:
+                        Console.WriteLine("Invalid choice!");
+                        break;
+                }
+            }
+            while (choice != 3);
+        }
+    }
+
+}
