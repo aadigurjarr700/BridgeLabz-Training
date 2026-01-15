f63316592a598b4e25eb86097eb8dcff3ed37985
aadigurjarr700 <aditya.gurjar_cs22@gla.ac.in>
2026-01-15 12:17:25 +0530

    [aditya] solved.uc1,uc2,uc3,uc4,uc5,uc6,uc7,uc8,uc9

--- oops-csharp-practice/scenario-based/AddressBookSystem/AddressBookMain.cs
+++ oops-csharp-practice/scenario-based/AddressBookSystem/AddressBookMain.cs
@@ -1,12 +1,17 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
-namespace BridgeLabzDup.oops_csharp_practice.scenario_based.address_book_system
+namespace BridgeLabzTraining.oops_csharp_practice.scenario_based.address_book
 {
     internal class AddressBookMain
     {
+        // Entry point of the Address Book application
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Multiple Address Book System");
+            Console.WriteLine("Welcome to Address Book Program\n"); // Greeting message
+
+            // Display the main menu and handle user choices
             MenuManager.DisplayMenu();
         }
     }
