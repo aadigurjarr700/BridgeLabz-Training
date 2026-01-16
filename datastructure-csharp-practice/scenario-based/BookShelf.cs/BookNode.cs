107e61e553ae7a20dc54920f1fcc429c218bb908
aadigurjarr700 <aditya.gurjar_cs22@gla.ac.in>
2026-01-16 16:09:41 +0530

    [aditya] solved.Book Shelf scenario

---
+++ datastructure-csharp-practice/scenario-based/BookShelf.cs/BookNode.cs
@@ -0,0 +1,14 @@
+﻿using System;
+ namespace BridgeLabzTraining.datastructure_csharp_practice.scenario_based.book_shelf;
+// Represents a single book in a linked list
+public class BookNode
+{
+    public string BookName;
+    public BookNode Next;
+
+    public BookNode(string bookName)
+    {
+        BookName = bookName;
+        Next = null;
+    }
+}
---
+++ datastructure-csharp-practice/scenario-based/BookShelf.cs/IBookShelfManager.cs
@@ -0,0 +1,9 @@
+﻿using System;
+ namespace BridgeLabzTraining.datastructure_csharp_practice.scenario_based.book_shelf;
+// Defines operations for library management
+public interface IBookShelfManager
+{
+    void AddBook(string genre, string bookName);
+    void BorrowBook(string genre, string bookName);
+    void ShowCatalog();
+}
